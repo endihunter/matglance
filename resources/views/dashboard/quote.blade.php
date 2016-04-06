7f1aa662a04912daf6862aeedeb8911f2acbea67
endi <endi1982@gmail.com>
2016-04-06 15:24:41 +0300

    improve quote markup

--- resources/views/dashboard/quote.blade.php
+++ resources/views/dashboard/quote.blade.php
@@ -1,18 +1,17 @@
 <div ng-controller="QuoteController">
     <card-box title="{{ trans('quote.title') }}" ng-init="quote={{ json_encode($quote) }}">
         <card-box-body>
-            <div class="text-muted">
+            <div class="well">
                 @{{ quote.quote }}
                 &nbsp;-&nbsp;
-                <span class="label label-info">@{{ quote.author }}</span>
+                <span class="label label-success">@{{ quote.author }}</span>
             </div>
             <hr>
-            <button ng-disabled="loading" class="btn btn-default" ng-click="fetchRandom()">
+            <button ng-disabled="loading" class="btn btn-info btn-sm" ng-click="fetchRandom()">
                 <i class="zmdi zmdi-refresh-sync"></i>
                 &nbsp;
                 {{ trans('quote.btn.more') }}
             </button>
-
         </card-box-body>
     </card-box>
 </div>

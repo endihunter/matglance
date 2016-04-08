9594dc8bac27a5bbd7ecfc40c3f559e93a2ed0d8
endi <endi1982@gmail.com>
2016-04-08 15:38:43 +0300

    fix paths to assets

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,23 +1,23 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
-    <meta name="author" content="Coderthemes">
+    <meta name="description" content="">
+    <meta name="author" content="Terranet.md">
 
     <!-- App Favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
     <!-- App title -->
     <title>{{ config('app.name') }}</title>
 
     <!-- App CSS -->
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/core.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/components.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/icons.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/pages.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/menu.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/responsive.css" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/core.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/components.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/pages.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/menu.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" type="text/css"/>
 
     <link rel="stylesheet" href="{{ elixir('css/helpers.css') }}">
 
@@ -29,6 +29,6 @@
     <![endif]-->
 
 
-    <script src="/assets/js/modernizr.min.js"></script>
+    <script src="{{ asset('assets/js/modernizr.min.js') }}"></script>
 
 </head>
--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -3,19 +3,19 @@
 </script>
 
 <!-- jQuery  -->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/detect.js"></script>
-<script src="assets/js/fastclick.js"></script>
-<script src="assets/js/jquery.slimscroll.js"></script>
-<script src="assets/js/jquery.blockUI.js"></script>
-<script src="assets/js/waves.js"></script>
-<script src="assets/js/jquery.nicescroll.js"></script>
-<script src="assets/js/jquery.scrollTo.min.js"></script>
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/detect.js') }}"></script>
+<script src="{{ asset('assets/js/fastclick.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.blockUI.js') }}"></script>
+<script src="{{ asset('assets/js/waves.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.nicescroll.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.scrollTo.min.js') }}"></script>
 
 <!-- App js -->
-<script src="assets/js/jquery.core.js"></script>
-<script src="assets/js/jquery.app.js"></script>
+<script src="{{ asset('assets/js/jquery.core.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.app.js') }}"></script>
 
 <script src="{{ asset('js/vendor.js') }}"></script>
 <script src="{{ elixir('js/app.js') }}"></script>

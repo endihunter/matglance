648b48c5fbad717775007c743de2bb421dcfa5c3
endi <endi1982@gmail.com>
2016-04-14 13:54:56 +0300

    skycons also blinks

--- public/build/js/app-fd17458de1.js
+++ public/build/js/app-fd17458de1.js
@@ -604,19 +604,14 @@ app.directive('skycon', function () {
         link: function (scope, element, attribs) {
             scope.size = attribs.size || 128;
 
-            var initIcon = function (createNew) {
+            var initIcon = function () {
                 var skycons = new Skycons({'color': 'grey'});
 
                 skycons.remove("skycon");
 
                 // you can add a canvas by it's ID...
                 var draw = attribs.icon.split('-').join('_').toUpperCase();
-                // if (createNew) {
-                //     skycons.add('skycon', Skycons[draw]);
-                // } else {
-                //     skycons.set('skycon', Skycons[draw]);
-                // }
-                skycons.set('skycon', Skycons[draw]);
+                skycons.add('skycon', Skycons[draw]);
 
                 // start animation!
                 skycons.play();

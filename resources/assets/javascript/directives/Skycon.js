9e88ad1b44928c21d40b2d996e6d4341320b3c22
endi <endi1982@gmail.com>
2016-04-14 13:40:13 +0300

    Fix skycons flickering

--- resources/assets/javascript/directives/Skycon.js
+++ resources/assets/javascript/directives/Skycon.js
@@ -8,17 +8,21 @@ app.directive('skycon', function () {
         link: function (scope, element, attribs) {
             scope.size = attribs.size || 128;
 
-            var initIcon = function () {
+            var initIcon = function (createNew) {
                 var skycons = new Skycons({'color': 'grey'});
 
                 // you can add a canvas by it's ID...
                 var draw = attribs.icon.split('-').join('_').toUpperCase();
-                skycons.add(document.getElementById('skycon'), Skycons[draw]);
+                if (createNew) {
+                    skycons.add('skycon', Skycons[draw]);
+                } else {
+                    skycons.set('skycon', Skycons[draw]);
+                }
 
                 // start animation!
                 skycons.play();
             };
-            initIcon();
+            initIcon(false);
 
             attribs.$observe('icon', initIcon);
         },

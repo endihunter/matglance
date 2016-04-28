ea4b9cd7929c0c2208f43e6db2dd170f837c0788
endi <endi1982@gmail.com>
2016-04-28 16:52:23 +0300

    remove online circle

--- resources/views/partials/_user_box.blade.php
+++ resources/views/partials/_user_box.blade.php
@@ -3,7 +3,7 @@
         <a target="_blank" href="{{ ($userUrl = auth()->user()->url ? : '#') }}">
             <img src="{{ auth()->user()->avatar ?: '/assets/images/users/avatar-1.jpg' }}" alt="user-img" title="Mat Helme" class="img-circle img-thumbnail img-responsive"/>
         </a>
-        <div class="user-status online"><i class="zmdi zmdi-dot-circle"></i></div>
+        {{--<div class="user-status online"><i class="zmdi zmdi-dot-circle"></i></div>--}}
     </div>
 
     <h5><a target="_blank" href="{{ $userUrl }}">{{ auth()->user()->name }}</a></h5>

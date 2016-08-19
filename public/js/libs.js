32f7f9d782822fb4a10fb82b1689102d96f7243e
yordanovbg <jordanovbiser@gmail.com>
2016-08-19 11:56:39 +0300

    leftsidebar  and content resizable if menu shown

--- public/js/libs.js
+++ public/js/libs.js
@@ -4215,12 +4215,18 @@ $(document).ready(function () {
     var menuHidden = true;
     $('.ti-menu').on('click', function () {
         if(menuHidden == true) {
-            $('.user-box').animate({'margin-left': '0px'});
-            $('#sidebar-menu').animate({'margin-left': '0px'});
+            // $('.user-box').animate({'margin-left': '0px'});
+            // $('#sidebar-menu').animate({'margin-left': '0px'});
+            $('.topbar .topbar-left').animate({'margin-left': '0px'}, 200);
+            $('.side-menu').animate({'margin-left': '0px'}, 200);
+            $('.content-page').animate({'margin-left': '250px'}, 200);
             menuHidden = !menuHidden;
         } else {
-            $('.user-box').animate({'margin-left': '-800px'});
-            $('#sidebar-menu').animate({'margin-left': '-800px'});
+            // $('.user-box').animate({'margin-left': '-800px'});
+            // $('#sidebar-menu').animate({'margin-left': '-800px'});
+            $('.topbar .topbar-left').animate({'margin-left': '-210px'}, 200);
+            $('.side-menu').animate({'margin-left': '-210px'}, 200);
+            $('.content-page').animate({'margin-left': '40px'}, 200);
             menuHidden = !menuHidden;
         }
     })

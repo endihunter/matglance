a1fefa6d30afa0e61ae03c13e8811de2fad8a1da
yordanovbg <jordanovbiser@gmail.com>
2016-08-22 15:47:15 +0300

    links from username and avatar removed

--- resources/views/partials/_user_box.blade.php
+++ resources/views/partials/_user_box.blade.php
@@ -1,10 +1,10 @@
 <div class="user-box">
     <div class="user-img">
-        <a target="_blank" href="{{ ($userUrl = auth()->user()->url ? : '#') }}">
+
             <img src="{{ auth()->user()->avatar ?: '/assets/images/users/avatar-1.jpg' }}" alt="user-img" title="Mat Helme" class="img-circle img-thumbnail img-responsive"/>
-        </a>
+
         {{--<div class="user-status online"><i class="zmdi zmdi-dot-circle"></i></div>--}}
     </div>
 
-    <h5 id="sidebar_user_name"><a target="_blank" href="{{ $userUrl }}">{{ auth()->user()->name }}</a></h5>
+    <h5 id="sidebar_user_name">{{ auth()->user()->name }}</h5>
 </div>

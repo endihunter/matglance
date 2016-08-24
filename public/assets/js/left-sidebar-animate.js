d0839caa490995595161ebe74172dd37a1615caa
yordanovbg <jordanovbiser@gmail.com>
2016-08-24 13:38:03 +0300

    more margin for long names 3

--- public/assets/js/left-sidebar-animate.js
+++ public/assets/js/left-sidebar-animate.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
             $('.content-page').animate({'margin-left': '250px'}, 200);
             menuHidden = !menuHidden;
         } else {
-            $('#sidebar_user_name').animate({'margin-left': '-800px'}, 50);
+            $('#sidebar_user_name').animate({'margin-left': '-800px'}, 200);
             $('.topbar .topbar-left').animate({'margin-left': '-210px'}, 200);
             $('.side-menu').animate({'margin-left': '-210px'}, 200);
             $('.content-page').animate({'margin-left': '40px'}, 200);
